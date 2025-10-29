3e96d256215f09fca336a1abf1af2506855203bd
Sybrant-2025 <ryker.scott@sybrantdata.com>
2025-10-29 17:34:09 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1800,7 +1800,7 @@ def serve_cfobridge_widget():
 @app.route('/newcfobridge')
 def serve_newcfobridge_widget():
     agent_id = request.args.get('agent', 'YOUR_DEFAULT_AGENT_ID')
-    js = serve_widget_js_updated4(agent_id, branding="Powered by cfobridge", brand="demo")
+    js = serve_widget_js_updated3(agent_id, branding="Powered by cfobridge", brand="demo")
     return Response(js, mimetype='application/javascript')
 
 @app.route('/voiceassistant')
