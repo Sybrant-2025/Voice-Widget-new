6047dde717bdcebb36675fee4f92f78fdcad6b53
Sybrant-2025 <ryker.scott@sybrantdata.com>
2025-11-06 11:22:18 +0530

    Update app.py

--- app.py
+++ app.py
@@ -36,7 +36,7 @@ BRAND_TO_WEBHOOK = {
     "dhilaktest": "https://script.google.com/macros/s/AKfycbz_VKhTD7G2Iqqk-ZrqOkebX-sXncZiqhxP12EOv3xgMvCchPbZ4n6PHPdTOKA7spwnKw/exec",
     "kopiko": "https://script.google.com/macros/s/AKfycbzip7wk995Q8BfktpVNZp6uJREQ8CqydyTVtxlTG0NucPugFOECa6XBpqo3Xv6pAkgM/exec",
     "leaserush" :"https://script.google.com/a/macros/sybrantdata.com/s/AKfycbxt20fVA71fAuePCWGzoRB-KRhjmpoogQF62Yr_qmFlqAP0wUQBkeNLJzlr9CrosIo9/exec",
-	"demo": "https://script.google.com/macros/s/AKfycbzLKBqFuQsk0S9xjezNQ7nKf-kDC_VHseNrezhXdQ9XrnBZRHpGz1mWyMT6ShGMcrH1ng/exec",
+	"demo": "https://script.google.com/macros/s/AKfycbxAiueSDZam9rAtxIbKsPggp5BORv0jW6eWN7HxywRDXDMvA3EiBab3ZFjQQsgwQjuRWg/exec",
 	"newgendigital": "https://script.google.com/macros/s/AKfycbyKHdnaO1IFWQSkpJiV-_dIZ6PU9GC-oRNwb8JjW6RM-DVVCcwScy3qTrG0ltRyH5Dc/exec",
 }
 
